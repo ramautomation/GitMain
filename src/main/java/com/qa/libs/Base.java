0bdb97a459822193b55605091054b0d1c503a4a8
ramautomation <rram1992.cbe@gmail.com>
2018-02-11 18:40:27 +0530

    first Repo

---
+++ src/main/java/com/qa/libs/Base.java
@@ -0,0 +1,9 @@
+package com.qa.libs;
+
+public class Base {
+	public void base()
+	{
+		System.out.println("Ram");
+	}
+
+}
---
+++ src/main/java/com/qa/page/Child.java
@@ -0,0 +1,15 @@
+package com.qa.page;
+
+public class Child {
+	
+	public void child()
+	{
+		System.out.println("Ram");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
